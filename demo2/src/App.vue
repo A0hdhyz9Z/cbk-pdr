a21419996d932d0bcda7dd944473276a34aa1495
2074434233@qq.com <2074434233@qq.com>
2022-10-11 19:00:07 +0800

    展示界面侧边导航栏

--- demo2/src/App.vue
+++ demo2/src/App.vue
@@ -8,9 +8,12 @@
         <router-link to="/">Go To Home</router-link>
         <router-link to="/login">Go To Login</router-link>
         <router-link to="/register">Go To Register</router-link>
+        <router-link to="/applayout">Go To AppLayout</router-link>
     </p>
     <router-view></router-view>
     </div>
 </template>
 
-<style></style>
+<style>
+    @import './style/common.scss';
+</style>
--- demo2/src/router/index.js
+++ demo2/src/router/index.js
@@ -4,13 +4,15 @@ import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
 import NotFound from '../views/NotFound.vue'
+import AppLayout from '../views/AppLayout.vue'
 
 
 const routes = [
   {path:'/',component:Home},
   {path:'/Login',component:Login},
   {path:'/Register',component:Register},
-  {path: '/path(.*)',component:NotFound}
+  {path: '/path(.*)',component:NotFound},
+  {path: '/AppLayout',component:AppLayout}
 
 ]
 
